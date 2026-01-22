b90fc91b90881363df8997d21ce6c58818f45425
Adrian YR <adrincon@unal.edu.co>
2026-01-22 11:11:48 -0500

    refactor(model): add Lombok annotations and remove manual getters/setters

--- src/main/java/com/example/hexagonalarchitecture/user/domain/model/Usuario.java
+++ src/main/java/com/example/hexagonalarchitecture/user/domain/model/Usuario.java
@@ -1,44 +1,17 @@
 package com.example.hexagonalarchitecture.user.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Usuario {
 
     private String nombre;
     private String email;
     private String password;
 
-    // Constructor
-    public Usuario(String nombre, String email, String password){
-        this.nombre = nombre;
-        this.email = email;
-        this.password = password;
-    }
-
-    // Getter y Setter para nombre
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    // Getter y Setter para email
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // Getter y Setter para password
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
 
